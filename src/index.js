c7a43b62a488f8d676c89c78073b2620cae9c358
thiago lopes de mello <timabuntu@gmail.com>
2022-11-22 18:14:56 -0300

    feat: created server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 const http = require("http");
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.end("<h1>Hello NodeJs</h1>");
+});
