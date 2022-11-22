b556b274e6b2115dc8c5ce9e62bf19302746014f
thiago lopes de mello <timabuntu@gmail.com>
2022-11-22 20:06:59 -0300

    feat: user route created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end('<h1>Hello NodeJs</h1>');
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+
+  if (request.url === '/users' && request.method === 'GET') {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end('<h1>Users</h1>');
+  }
+
+  // response.writeHead(200, { 'Content-Type': 'text/html' });
+  // response.end('<h1>Ola NodeJs</h1>');
 });
 
 server.listen(3000, () =>
