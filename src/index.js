8cbfec19a54e8b5c316286238d8a386c8735c1b5
thiago lopes de mello <timabuntu@gmail.com>
2022-11-22 19:56:34 -0300

    style: added single quote

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const http = require("http");
+const http = require('http');
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.end("<h1>Hello NodeJs</h1>");
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.end('<h1>Hello NodeJs</h1>');
 });
 
 server.listen(3000, () =>
-  console.log("🔥 Server started at http://localhost:3000")
+  console.log('🔥 Server started at http://localhost:3000')
 );
