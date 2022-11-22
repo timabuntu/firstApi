2b4b29ea6597177f1a7da163812b33519300d4c0
thiago lopes de mello <timabuntu@gmail.com>
2022-11-22 18:16:06 -0300

    feat: server up

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ const server = http.createServer((request, response) => {
   response.writeHead(200, { "Content-Type": "text/html" });
   response.end("<h1>Hello NodeJs</h1>");
 });
+
+server.listen(3000, () =>
+  console.log("🔥 Server started at http://localhost:3000")
+);
