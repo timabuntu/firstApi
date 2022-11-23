112c9b009fadd773ec8beed65fe8fdcb3e5ca0cc
thiago lopes de mello <timabuntu@gmail.com>
2022-11-22 21:39:52 -0300

    feat: show users created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 const http = require('http');
 
-const user = require('./mocks/users');
+const users = require('./mocks/users');
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
   if (request.url === '/users' && request.method === 'GET') {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end('<h1>Users</h1>');
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify(users));
   }
-
-  // response.writeHead(200, { 'Content-Type': 'text/html' });
-  // response.end('<h1>Ola NodeJs</h1>');
 });
 
 server.listen(3000, () =>
