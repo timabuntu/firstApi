62a2bbb55d5fab6e6334ca3a18ea53e86a3a8463
thiago lopes de mello <timabuntu@gmail.com>
2022-11-22 21:40:36 -0300

    feat: show route not found created

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const server = http.createServer((request, response) => {
   if (request.url === '/users' && request.method === 'GET') {
     response.writeHead(200, { 'Content-Type': 'application/json' });
     response.end(JSON.stringify(users));
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end(`Cannot ${request.method} ${request.url}`);
   }
 });
 
